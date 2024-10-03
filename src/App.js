0a4da4d893e89e432b43fe6255ffa020df3dbadc
Donald Macritchie <donald.macritchie@outlook.com>
2024-10-03 12:48:16 +0100

    add Hashrouter rather tahn browser router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import PageTransitionWrapper from './components/PageTransitionWrapper';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="min-h-screen flex flex-col">
         <Navbar />
-        {/* The page transitions are handled inside PageTransitionWrapper */}
         <PageTransitionWrapper />
         <Footer />
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
