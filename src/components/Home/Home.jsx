425e89938d39870719d576e7eddb4af877cfd9db
Donald Macritchie <donald.macritchie@outlook.com>
2024-10-03 12:16:04 +0100

    change home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,14 @@
-import React from "react";
-import { HiArrowNarrowRight } from "react-icons/hi";
-import { Link } from "react-router-dom";
-import { useLayoutEffect, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { homeAnimations } from "./homeAnimations";
-
 
 const Home = () => {
   const comp = useRef(null);
-  const overlayTextRef = useRef(null); // Use ref for overlay text
   const [overlayHidden, setOverlayHidden] = useState(false);
 
   useLayoutEffect(() => {
-    // Call the GSAP animation function and pass necessary arguments
+    console.log("Starting home animations"); // Debugging line
     const cleanup = homeAnimations(setOverlayHidden, comp);
-
-    return cleanup; // Clean up the GSAP context on unmount
+    return cleanup;
   }, []);
 
   return (
@@ -27,8 +21,7 @@ const Home = () => {
         >
           <h1
             id="overlay-text"
-            ref={overlayTextRef} // Assign the ref here
-            className="text-6xl sm:text-7xl font-bold text-white border-2 border-white p-2 rounded-md" // Text is initially black, will change to white
+            className="text-6xl sm:text-7xl font-bold text-white border-2 border-white p-2 rounded-md"
           >
             D.M
           </h1>
@@ -36,22 +29,11 @@ const Home = () => {
       )}
 
       {/* Main Content */}
-      <div
-        id="main-content"
-        className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center mt-[300px]"
-      >
-        <p id="hi-my-name-is" className="text-2xl">
-          Hi, My name is
-        </p>
-        <h1 id="name" className="text-6xl sm:text-7xl font-bold text-black">
-          Donald MacRitchie
-        </h1>
-        <h2 id="title-1" className="text-4xl sm:text-7xl">
-          Junior Fullstack
-        </h2>
-        <h2 id="title-2" className="text-4xl sm:text-7xl">
-          Web Developer
-        </h2>
+      <div id="main-content" className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center mt-[300px]">
+        <p id="hi-my-name-is" className="text-2xl">Hi, My name is</p>
+        <h1 id="name" className="text-6xl sm:text-7xl font-bold text-black">Donald MacRitchie</h1>
+        <h2 id="title-1" className="text-4xl sm:text-7xl">Junior Fullstack</h2>
+        <h2 id="title-2" className="text-4xl sm:text-7xl">Web Developer</h2>
         <div className="max-w-[200px]">
           <Link
             id="view-projects-btn"
